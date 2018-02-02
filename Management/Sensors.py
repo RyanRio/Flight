249d2223bd0a0e08119b5fa03136537349007552
Ryan Mitchell <ryan.mitchell@spsnet.sps.edu>
2018-02-02 11:55:02 -0500

    stuff

--- Management/Sensors.py
+++ Management/Sensors.py
@@ -1,24 +1,26 @@
 """ provides functionality for managing IC2 and Digital Sensors for the drone """
 import RPi.GPIO as GPIO
-
+from smbus import SMBus
 
 class GPIOManager():
   """ functionality class """
 
   """ list of i2c addresses """
-  addresses = []
+  i2cDict = {}
 
   outChannels = [] 
   inChannels = []
   QuickWrites = []
-  """ if i2c then include addressses that you are going to use, makes setup easier and furthur use """
-  def __init__(self, addresses=[]):
-    self.addresses = addresses
-  def addAdress(self, address):
-    self.addresses.append(address)
 
-  def bindQuickWrite(self, addressName):
-    self.QuickWrites.append()
+  """ if i2c then include addressses that you are going to use, makes setup easier and furthur use """
+  def __init__(self, i2cDictInputs = [["NAN"],["NAN"]]):
+    self.addresses = addresses
+    if(i2cDictInputs[0][0]!="NAN"):
+          for i in len(i2cDictInputs[0]):
+              self.i2cDict[i2cDictInputs[0][i]] = SMBus(i2cDictInputs[1][i])
+
+  def addAdress(self, address):
+    self.i2cDict[address[0]] = SMBus(address[1])
 
   def addOutConnections(self, channelArray):
     """ channelArray is of type [channels], type is OUT """
@@ -35,6 +37,7 @@ class GPIOManager():
   def addInConnections(self, channelArray):
     """ channelArray is of type [ [channelNumber, type], [channelNumber, type] ] where type is either UP or DOWN """
     try:
+
       for channel in channelArray:
         if channel[1] == "UP":
           GPIO.setup(channel[0], GPIO.IN, pull_up_down=GPIO.PUD_UP)
